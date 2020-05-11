e79b376b59adcb33f80bcfd8504f63b2343768d9
toladele <toladele99@gmail.com>
2020-05-11 16:36:12 -0400

    Implement title bar, background colours and 8 ball picture

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball1.png'),
+        onPressed: () {
+          print('clicked');
+        },
+      ),
+    );
+  }
+}
